76ae613a8940870182a6679a0473b0a6b25b7ef4
saouk <honggalra2@gmail.com>
2020-11-22 21:51:13 +0900

    model enum setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :user_descriptions
+
+  enum status: {
+      customer: 0,
+      seller: 10,
+      unregister: 20
+  }
 end
--- app/models/user_description.rb
+++ app/models/user_description.rb
@@ -1,2 +1,16 @@
 class UserDescription < ApplicationRecord
+  belongs_to :user
+
+  enum grade: {
+      level_1: 1,
+      level_2: 2,
+      level_3: 3,
+      level_4: 4
+  }
+
+  enum gender: {
+      boy: 1,
+      girl: 2,
+      no_gender: 3
+  }
 end
